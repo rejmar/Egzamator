53fbab8bf86e6c02a6051dee84f9f8fcc385e570
marjusz <maarcinrej@gmail.com>
2019-02-11 21:31:58 +0100

    Added Lombok library dependency and implemented it in Mark and Question DAO

--- src/main/java/com/mr/egzamator/model/Mark.java
+++ src/main/java/com/mr/egzamator/model/Mark.java
@@ -1,47 +1,27 @@
 package com.mr.egzamator.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Data
 @Table(name = "marks")
+@NoArgsConstructor
 public class Mark {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_mark")
     private int id;
+
+    @Column(name = "mark")
     private double mark;
     private Test test;
     private Student student;
 
-
-//    public Mark(double mark) {
-//        this.mark = mark;
-//
-//    }
-
-    public Mark(){
-
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id_mark")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Column(name = "mark")
-    public double getMark() {
-        return mark;
-    }
-
-    public void setMark(double mark) {
-        this.mark = mark;
-    }
 
     @OneToOne
     @JoinColumn(name = "tests_id_test")
@@ -49,17 +29,9 @@ public class Mark {
         return test;
     }
 
-    public void setTest(Test test) {
-        this.test = test;
-    }
-
     @ManyToOne
     @JoinColumn(name = "students_id_student")
     public Student getStudent() {
         return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
     }
 }
--- src/main/java/com/mr/egzamator/model/Question.java
+++ src/main/java/com/mr/egzamator/model/Question.java
@@ -1,106 +1,44 @@
 package com.mr.egzamator.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name = "questions")
 public class Question {
-
-    private int id;
-    private String content;
-    private String ans_a;
-    private String ans_b;
-    private String ans_c;
-    private String ans_d;
-    private char correct_answer;
-    private Test test;
-
-//    public Question(String content, String ans_a, String ans_b, String ans_c, String ans_d, char correct_answer, Test test) {
-//        this.content = content;
-//        this.ans_a = ans_a;
-//        this.ans_b = ans_b;
-//        this.ans_c = ans_c;
-//        this.ans_d = ans_d;
-//        this.correct_answer = correct_answer;
-//        this.test = test;
-//    }
-
-    public Question(){
-
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_question")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    private int id;
 
     @Column(name = "content")
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private String content;
 
     @Column(name = "ans_a")
-    public String getAns_a() {
-        return ans_a;
-    }
-
-    public void setAns_a(String ans_a) {
-        this.ans_a = ans_a;
-    }
+    private String ans_a;
 
     @Column(name = "ans_b")
-    public String getAns_b() {
-        return ans_b;
-    }
-
-    public void setAns_b(String ans_b) {
-        this.ans_b = ans_b;
-    }
+    private String ans_b;
 
     @Column(name = "ans_c")
-    public String getAns_c() {
-        return ans_c;
-    }
-
-    public void setAns_c(String ans_c) {
-        this.ans_c = ans_c;
-    }
+    private String ans_c;
 
     @Column(name = "ans_d")
-    public String getAns_d() {
-        return ans_d;
-    }
-
-    public void setAns_d(String ans_d) {
-        this.ans_d = ans_d;
-    }
+    private String ans_d;
 
     @Column(name = "correct_answer")
-    public char getCorrect_answer() {
-        return correct_answer;
-    }
+    private char correct_answer;
 
-    public void setCorrect_answer(char correct_answer) {
-        this.correct_answer = correct_answer;
-    }
+    private Test test;
 
     @ManyToOne
     @JoinColumn(name = "tests_id_test")
     public Test getTest() {
         return test;
-    }
-
-    public void setTest(Test test) {
-        this.test = test;
     }
 }
