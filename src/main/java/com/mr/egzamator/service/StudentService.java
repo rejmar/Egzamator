2bd6c3becfde02a897874ada2e676d350b0feaa3
Mari <maarcinreJ@gmail.com>
2019-09-14 16:27:50 +0200

    Changed from Maven to Gradle

--- src/main/java/com/mr/egzamator/service/StudentService.java
+++ src/main/java/com/mr/egzamator/service/StudentService.java
@@ -5,7 +5,6 @@ import com.mr.egzamator.respository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @Service
--- src/test/java/com/mr/egzamator/EgzamatorApplicationTests.java
+++ src/test/java/com/mr/egzamator/EgzamatorApplicationTests.java
@@ -1,16 +1,13 @@
 package com.mr.egzamator;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class EgzamatorApplicationTests {
+class EgzamatorApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
